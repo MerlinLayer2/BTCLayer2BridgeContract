f9d02fbae6d481b2a7786fcb816fce213bbdd29c
turingczz <920886811@163.com>
2024-02-01 22:02:55 +0800

    debug

--- contracts/BTCLayer2BridgeERC721.sol
+++ contracts/BTCLayer2BridgeERC721.sol
@@ -63,12 +63,9 @@ contract BTCLayer2BridgeERC721 is OwnableUpgradeable {
     }
 
     //tokenURI: id->number->tokenURI(number)
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        uint256 inscriptionNumber = tokenId;
+    function tokenURI(uint256 inscriptionNumber) public view virtual override returns (string memory) {
         require(inscriptionNumber>=1, "This inscriptionNumber is not exist");
-
-        bytes memory inscriptionId = mpNumber2Id[inscriptionNumber];
-        return super.tokenURI(inscriptionId);
+        return super.tokenURI(mpNumber2Id[inscriptionNumber]);
     }
 
     function batchMintERC721Token(bytes32 txHash, address token, address to, string[] memory inscriptionIds, uint256[] memory inscriptionNumbers) external onlyBridge {
