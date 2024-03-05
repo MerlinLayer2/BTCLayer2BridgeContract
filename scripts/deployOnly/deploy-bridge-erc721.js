2ba44db0e7bd57015cd7c5175d2f248d8c86e71e
turingczz <920886811@163.com>
2024-03-05 12:41:09 +0800

    feat: add bridge-erc20-erc721 single deploy

--- scripts/deployOnly/deploy-bridge-erc721.js
+++ scripts/deployOnly/deploy-bridge-erc721.js
@@ -9,14 +9,14 @@ let deployOutput = {};
 if (fs.existsSync(pathOutputJson)) {
     deployOutput = require(pathOutputJson);
 }
-console.log(`Bridge Contract Proxy Addr: ${deployOutput.bTCLayer2BridgeDeployContract}`)
+console.log(`BridgeERC721 Contract Proxy Addr: ${deployOutput.bTCLayer2BridgeERC721DeployContract}`)
 
 async function main() {
     let [owner] = await ethers.getSigners();
     console.log(`Using owner account: ${await owner.getAddress()}`)
 
     // 1. Get the contract to deploy
-    const BridgeFactory = await ethers.getContractFactory("BTCLayer2Bridge", owner);
+    const BridgeFactory = await ethers.getContractFactory("BTCLayer2BridgeERC721", owner);
     console.log('Deploying ...');
 
     // 2. Instantiating a new Box smart contract
@@ -26,7 +26,7 @@ async function main() {
     await bridge.waitForDeployment();
 
     // 4. Use the contract instance to get the contract address
-    console.log('bridge deployed to:', bridge.target);
+    console.log('bridge-erc721 deployed to:', bridge.target);
 }
 
 main().catch((error) => {
@@ -35,6 +35,6 @@ main().catch((error) => {
 });
 
 // deploy+verify.
-// cmd1: npx hardhat run scripts/deployOnly/deploy-bridge.js --network btclayer2
+// cmd1: npx hardhat run scripts/deployOnly/deploy-bridge-erc721.js --network btclayer2
 // cmd2: npx hardhat verify --network btclayer2 0xB354DE4A8072BBD6e32bB152D72287475CAAeEDe
 
