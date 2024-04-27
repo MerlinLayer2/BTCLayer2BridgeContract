4776a22b3d4a146db11a4f1706971dc1607a483f
turingczz <920886811@163.com>
2024-04-27 14:18:53 +0800

    feat: mod         mapping(address => stRate) whiteList;

--- contracts/WhiteLists.sol
+++ contracts/WhiteLists.sol
@@ -14,20 +14,20 @@ library Whites {
     }
 
     //_address is msg.sender or token.
-    function setWhiteList(White storage white, address _address, uint256 _rate) public {
+    function setWhiteList(White storage white, address _address, uint256 _rate) internal {
         require(_address != address (0), "invalid _address");
         require(_rate >= 0, "invalid _rate");
 
         white.whiteList[_address] = stRate(true, _rate);
     }
 
-    function deleteWhiteList(White storage white, address _address) public {
+    function deleteWhiteList(White storage white, address _address) internal {
         require(_address != address (0), "invalid _address");
         delete white.whiteList[_address];
     }
 
 
-    function getBridgeFee(White storage white, address msgSender, address token, uint256 bridgeFee) public view   returns(uint256) {
+    function getBridgeFee(White storage white, address msgSender, address token, uint256 bridgeFee) internal view   returns(uint256) {
         if (white.whiteList[msgSender].isSet) {
             return bridgeFee * white.whiteList[msgSender].rate / 100;
         }
@@ -39,7 +39,7 @@ library Whites {
         return bridgeFee;
     }
 
-    function getBridgeFeeTimes(White storage white, address msgSender, address token, uint256 times, uint256 bridgeFee) public view  returns(uint256) {
+    function getBridgeFeeTimes(White storage white, address msgSender, address token, uint256 times, uint256 bridgeFee) internal view  returns(uint256) {
         return getBridgeFee(white, msgSender, token, bridgeFee) * times;
     }
 }
